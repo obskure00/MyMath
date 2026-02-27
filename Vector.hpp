487499a1179266b74c7418dffea7aef46c45e45d
Manuel <manuelwro33@gmail.com>
2026-02-27 14:26:29 +0100

    first commit

---
+++ Vector.hpp
@@ -0,0 +1,16 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <memory>
+#include <vector>
+
+class Vector3 {
+    private:
+        double x;
+        double y;
+        double z;
+
+    public:
+};
+
+#endif
