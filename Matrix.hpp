09c32a926e53fcbdc1c989002315e42c3bad478e
Manuel <manuelwro33@gmail.com>
2026-02-28 14:23:07 +0100

    Wrote constructors for Matrix3x3

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,7 +1,42 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
-#endif
+#include <array>
+#include <initializer_list>
+#include <algorithm>
+#include <stdexcept>
 
-class Matrix3 {
+class Matrix3x3 {
+private:
+    std::array<double, 9> data;
+
+public:
+
+    Matrix3x3() {
+        data.fill(0.0);
+    }
+
+    explicit Matrix3x3(double initial) {
+        data.fill(initial);
+    }
+
+    Matrix3x3(std::initializer_list<double> list) {
+        if (list.size() != 9) {
+            throw std::invalid_argument("Matrix3x3 requires exactly 9 values.");
+        }
+        std::copy(list.begin(), list.end(), data.begin());
+    }
+
+    Matrix3x3(double m00, double m01, double m02,
+              double m10, double m11, double m12,
+              double m20, double m21, double m22) 
+              : data{m00, m01, m02, m10, m11, m12, m20, m21, m22} {}
+
+    static Matrix3x3 Identity() {
+        Matrix3x3 m;
+        m.data[0] = 1.0; m.data[4] = 1.0; m.data[8] = 1.0;
+        return m;
+    }
 };
+
+#endif
